412a480963d92c79a9c1730fbbcce7ec3c722da6
hyojeonglee <hyon2224@naver.com>
2017-01-20 12:41:42 +0900

    add thread

---
+++ Lab0120_Thread/src/kr/or/bank/Ex01_Single_Thread.java
@@ -0,0 +1,21 @@
+package kr.or.bank;
+
+// single process + single thread
+// JVM > stack 메모리 하나만 사용!
+
+public class Ex01_Single_Thread {
+	public static void main(String[] args) {
+		System.out.println("Main thread 시작");
+		worker();
+		worker2();
+		System.out.println("Main thread 종료");
+	}
+	
+	static void worker() {
+		System.out.println("1번");
+	}
+	
+	static void worker2() {
+		System.out.println("2번");
+	}
+}
