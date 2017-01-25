40b5769a475583027ecf1fa9876561e56760c476
hyojeonglee <hyon2224@naver.com>
2017-01-25 12:40:12 +0900

    add EmpProject

---
+++ Lab0125_EmpProject/src/DAO/DeptDAO.java
@@ -0,0 +1,10 @@
+package DAO;
+
+// Goal : 얘 혼자 db 작업(CRUD)
+// 함수(select, insert, update, delete)
+// Q. 한 테이블 대상으로 함수를 최소 몇 개 만들까? 최소 다섯개!
+// A. insert, update, delete 3개는 확정!
+//    select는 두 개 만들어야함! 1. 전체 select 2. 조건 select(PK 기반으로 쿼리 던지는 함수)
+public class DeptDAO {
+
+}
